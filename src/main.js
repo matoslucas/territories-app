448e53b3122d14d77df285aea6d107fe479f9d1d
matoslucas <matoslucas@hotmail.com>
2024-09-08 02:03:08 -0600

    update style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
-import App from './App.vue'
-import './scss/main.scss'  // Import global styles
+import App from './App.vue';
+import './scss/main.scss';  // Import global styles
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createApp(App).mount('#app')
